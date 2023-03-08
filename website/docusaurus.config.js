d18f1af232f7970e9e96386cdcb60c1e154fff79
Wanderson Alves <wandersonalwes@hotmail.com>
2023-03-08 11:14:21 -0300

    build: adjust base url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://wandersonalwes-react-hooks.netlify.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/react-hooks/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
